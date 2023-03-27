06d8f754087ff8f40a64321d89ddcda435a6971d
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-03-27 17:15:55 +0200

    test install docker

--- sae/config/script/installDocker.sh
+++ sae/config/script/installDocker.sh
@@ -8,7 +8,8 @@ mkdir -m 0755 -p /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
@@ -25,9 +26,9 @@ systemctl restart docker
 
 mv /home/user/config/fichierDocker/daemon.json /etc/docker/
 
-mkdir .docker
+mkdir /root/.docker
 
-mv /home/user/config/fichierDocker/config.json .docker/
+mv /home/user/config/fichierDocker/config.json /root/.docker/
 
 systemctl daemon-reload
 
