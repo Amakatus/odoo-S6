543536d07baa94bb937d06db3e692818adaed21f
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-11 16:47:48 +0200

    New version of save.sh

--- sae/config/script/installPostgres.sh
+++ sae/config/script/installPostgres.sh
@@ -7,3 +7,5 @@ echo "host    all     all     192.168.194.17/24        md5" >> /etc/postgresql/1
 sed -i -e "s/#listen_addresses.=.'localhost'/listen_addresses = '0.0.0.0'/g" /etc/postgresql/13/main/postgresql.conf
 
 systemctl restart postgresql
+
+mkdir /home/user/dbsave
--- sae/config/script/save.sh
+++ sae/config/script/save.sh
@@ -1,24 +1,6 @@
-if [ -a requestDB.txt]
-then
-    rm requestDB.txt
-fi
-
-if [ -a nameDB.txt]
-then
-    rm requestDB.txt
-fi
-
-psql -c 'select datname from pg_database;' > requestDB.txt
-
-nbLigne=$(wc -l requestDB.txt | cut -d ' ' -f 1)
-
-for ((i=3 ; i<$nbLigne-1 ; i++))
+for i in 'cat listeDB.txt'
 do
-    sed -n ''$i'p' test.txt >> nameDB.txt
-done 
-
-for j in $(cat nameDB.txt)
-do
-    pg_dump --format=custom --file $j.dump $j
+    pg_dump -h localhost -U admin --format=custom --file $i.dump $i;
 done
 
+mv ./* /home/user/dbsave
