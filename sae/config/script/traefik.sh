936fc3511ae83590ae407976c7cc559449d547e0
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-12 19:11:49 +0200

    test script traefik

--- sae/config/script/traefik.sh
+++ sae/config/script/traefik.sh
@@ -2,8 +2,6 @@
 
 docker network create net_traefik
 
-mkdir ./traefik
-
-mv /home/user/config/fichierOdoo/docker-compose-reverse.yml ./traefik/
+mv /home/user/config/fichierOdoo/docker-compose-reverse.yml ./
 
 docker compose up -d
