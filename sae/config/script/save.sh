077c3df9f5f8a8d00f288ed1972d3a6b92fa3c0c
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-12 09:12:18 +0200

    test

--- sae/config/script/save.sh
+++ sae/config/script/save.sh
@@ -1,6 +1,10 @@
-for i in 'cat listeDB.txt'
+for nomDB in 'cat listeDB.txt'
 do
-    pg_dump -h localhost -U admin --format=custom --file $i.dump $i;
+    pg_dump -h localhost -U admin --format=custom --file $nomDB.dump $nomDB;
 done
 
-mv ./* /home/user/dbsave
+dateDuJour=$(date +"Dates: %d-%m-%Y---%HH%MM%SS" | cut -b 8-)
+
+mkdir /home/user/dbsave/$dateDuJour
+
+mv ./* /home/user/dbsave/$dateDuJour
