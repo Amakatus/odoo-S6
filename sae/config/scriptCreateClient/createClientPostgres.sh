0684ae2786494ac28b1d4f7880395298da9c5783
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-13 15:47:55 +0200

    push

--- sae/config/scriptCreateClient/createClientPostgres.sh
+++ sae/config/scriptCreateClient/createClientPostgres.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd ../../../../
 
 echo Veuillez entrer le même mot de passe client que précèdement
 
-sudo su - postgres -c "createuser -P $1"
+sudo su - postgres -c psql -c "createuser -P $1"
 
 echo Veuillez entrer le même mot de passe client que précèdement
 sudo su - postgres -c "createdb -O $1 db$1"
