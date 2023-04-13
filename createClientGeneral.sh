280eb5e2b89f39e4ae59cdb2c70da71b061e4c86
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-13 20:46:44 +0200

    move createVm

--- createClientGeneral.sh
+++ createClientGeneral.sh
@@ -4,13 +4,8 @@ echo "Nom Client :" && read nomClient
 echo "Mot de passe Client:" && read mdpClient
 echo "Port" && read port
 
-<<<<<<< HEAD:sae/createClientGeneral.sh
 adressIPbd=$(cat ./config/ip/ipBD.txt)
 adressIPodoo=$(cat ./config/ip/ipBD.txt)
-=======
-adressIPbd=$(cat /home/user/config/ip/ipBD.txt)
-adressIPodoo=$(cat /home/user/config/ip/ipBD.txt)
->>>>>>> 8cd2735e04764bb00906355ebc77dc3595405e79:createClientGeneral.sh
 
 ssh -t user@$adressIPbd "su -c 'source ./config/scriptCreateClient/createClientPostgres.sh $nomClient'"
 ssh -t user@$adressIPodoo "su -c 'source ./config/scriptCreateClient/createClientOdoo.sh $nomClient $mdpClient $adressIPbd $port'"
