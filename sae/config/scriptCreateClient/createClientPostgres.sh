a811f988f1e219f01d3da39c2913b65e978c980a
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-13 16:00:46 +0200

    push

--- sae/config/scriptCreateClient/createClientPostgres.sh
+++ sae/config/scriptCreateClient/createClientPostgres.sh
@@ -2,7 +2,8 @@
 
 echo Veuillez entrer le même mot de passe client que précèdement
 
-su - postgres -c "createuser -P $1"
+#psql -h localhost -U admin -c "-c '\x' -c
+su - postgres -c '\x' -c "createuser -P $1"
 
 echo Veuillez entrer le même mot de passe client que précèdement
 su - postgres -c "createdb -O $1 db$1"
--- sae/config/scriptCreateVM/createSuperUser.sh
+++ sae/config/scriptCreateVM/createSuperUser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo ETAPE MANUELLE: entrer les commandes présentes dans la procèdure ou ci après
 echo Veuillez entrer le mot de passe du super utilisateur postgres "(" Attention cette utilisateur aura tous les droits sur les bases de données, donnez lui un mot de passe robuste")"
-
-sudo su - postgres -c "createuser -P -s $1"
+echo createuser -P -s admin
+echo Apr'ès avoir entré le mot de passe appuyé sur "CTRL+D"
+su - postgres 
