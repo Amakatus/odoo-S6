99e3a0a5c250045bd55fef9a139c6bc985e69ec3
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-13 18:33:46 +0200

    remise en ordre

--- config/scriptCreateClient/createClientOdoo.sh
+++ config/scriptCreateClient/createClientOdoo.sh
@@ -6,7 +6,7 @@ mkdir $1/config
 
 mkdir $1/odoo-data
 
-cp /home/user/config/fichierOdoo/docker-compose-odoo.yml $1/
+cp /home/user/config/fichierOdoo/docker-compose.yml $1/
 
 cp /home/user/config/fichierOdoo/odoo.conf $1/config/
 
@@ -15,12 +15,19 @@ sed -i -e "s/NOMUSER/$1/g" $1/config/odoo.conf
 sed -i -e "s/MDPUSER/$2/g" $1/config/odoo.conf
 sed -i -e "s/NOMDB/db$1/g" $1/config/odoo.conf
 
-sed -i -e "s/PORT/$4/g" $1/config/odoo.conf
-sed -i -e "s/EPAL/$1/g" $1/config/odoo.conf
+sed -i -e "s/PORT/$4/g" $1/docker-compose.yml
+sed -i -e "s/EPAL/$1/g" $1/docker-compose.yml
 
 cd $1
 
 echo Lorsque le chargement des addons seront fini faire : CTRL+C
+echo Lorsque le chargement des addons seront fini faire : CTRL+C
+echo Lorsque le chargement des addons seront fini faire : CTRL+C
+echo Lorsque le chargement des addons seront fini faire : CTRL+C
+echo Attention de ne pas le faire après les modules
+
+sleep 10
+
 docker compose run web -i db$1
 
 docker compose run -d
--- createClientGeneral.sh
+++ createClientGeneral.sh
@@ -4,8 +4,10 @@ echo "Nom Client :" && read nomClient
 echo "Mot de passe Client:" && read mdpClient
 echo "Port" && read port
 
-adressIPbd=$(cat ./home/user/config/ip/ipBD.txt)
-adressIPodoo=$(cat ./home/user/config/ip/ipBD.txt)
+adressIPbd=$(cat /home/user/config/ip/ipBD.txt)
+adressIPodoo=$(cat /home/user/config/ip/ipBD.txt)
 
 ssh -t user@$adressIPbd "su -c 'source ./config/scriptCreateClient/createClientPostgres.sh $nomClient'"
 ssh -t user@$adressIPodoo "su -c 'source ./config/scriptCreateClient/createClientOdoo.sh $nomClient $mdpClient $adressIPbd $port'"
+ssh -t user@$adressIPodoo "su -c 'source ./config/scriptCreateClient/lancerClient.sh $nomClient'"
+
