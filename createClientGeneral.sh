5a8243c9956b4cf333ff8d000ffd95044969d381
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-13 21:10:00 +0200

    o

--- createClientGeneral.sh
+++ createClientGeneral.sh
@@ -5,7 +5,7 @@ echo "Mot de passe Client:" && read mdpClient
 echo "Port" && read port
 
 adressIPbd=$(cat ./config/ip/ipBD.txt)
-adressIPodoo=$(cat ./config/ip/ipBD.txt)
+adressIPodoo=$(cat ./config/ip/ipODOO.txt)
 
 ssh -t user@$adressIPbd "su -c 'source ./config/scriptCreateClient/createClientPostgres.sh $nomClient'"
 ssh -t user@$adressIPodoo "su -c 'source ./config/scriptCreateClient/createClientOdoo.sh $nomClient $mdpClient $adressIPbd $port'"
