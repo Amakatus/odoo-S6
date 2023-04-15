b5e3179b302ee8dbdf3e76381a93c5c4238df74c
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2023-04-15 21:25:32 +0200

    Final i think

--- createClient.sh
+++ createClient.sh
@@ -8,7 +8,15 @@ adressIPbd=$(cat ./config/ip/ipBD.txt)
 adressIPodoo=$(cat ./config/ip/ipODOO.txt)
 virt=$(cat ./config/machinePhysique/virt.txt)
 
+echo Attention, vous avez chnagé de mot de passe précèdement
+echo Attention, vous avez chnagé de mot de passe précèdement
 ssh -t user@$adressIPbd "su -c 'source ./config/scriptCreateClient/createClientPostgres.sh $nomClient $adressIPodoo'"
+
+echo Attention, vous avez chnagé de mot de passe précèdement
+echo Attention, vous avez chnagé de mot de passe précèdement
 ssh -t user@$adressIPodoo "su -c 'source ./config/scriptCreateClient/createClientOdoo.sh $nomClient $mdpClient $adressIPbd $port $virt'"
+
+echo Attention, vous avez chnagé de mot de passe précèdement
+echo Attention, vous avez chnagé de mot de passe précèdement
 ssh -t user@$adressIPodoo "su -c 'source ./config/scriptCreateClient/lancerClient.sh $nomClient'"
 
